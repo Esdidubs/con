fbfa5063e873d437e31a66fbbecd61f3246990f2
Esdidubs <sward3644@gmail.com>
2021-02-17 14:55:40 -0700

    Added comments, converted some methods
    -Switched to .html function instead of replace
    -Added comments
    -Consolidated listSetup and calculateComparison using reusable code instead of if/else

--- app.js
+++ app.js
@@ -14,63 +14,48 @@ function buttons() {
 	$('#unitSelection').on('change', function() {
 		event.preventDefault();
 		listSetup();
-		$('#heightResults').replaceWith(`<div id="heightResults"></div>`);
+
+		// Clear the last conversion and the current number inputted
+		$('#heightResults').html('');
 		$('#unit').val('');
 	});
-}
+};
 
 // Populates the dropdowns based on the unit selected
 function listSetup() {
 	let listItems = '';
-	if ($('#unitSelection').val() == 'heightCompare') {
-		for (var key in heightConversions) {
-			listItems += `<option value="${key}">${heightConversions[key].name}</option>`;
-		}
-	} else if ($('#unitSelection').val() == 'weightCompare') {
-		for (var key in weightConversions) {
-			listItems += `<option value="${key}">${weightConversions[key].name}</option>`;
-		}
-	} else {
-		for (var key in volumeConversions) {
-			listItems += `<option value="${key}">${volumeConversions[key].name}</option>`;
-		}
+	let conversion = $('#unitSelection').val();
+
+	for (var key in conversions[conversion]) {
+		listItems += `<option value="${key}">${conversions[conversion][key].name}</option>`;
 	}
 
-	$('#heightCom1').replaceWith(` 
-        <select name="heightCom1" id="heightCom1" class="heightCom">
-            <option value="" disabled selected>Item 1</option>
-            ${listItems}
-        </select>`);
-	$('#heightCom2').replaceWith(` 
-        <select name="heightCom2" id="heightCom2" class="heightCom">
-            <option value="" disabled selected>Item 2</option>
-            ${listItems}
-        </select>`);
-}
+	$('#heightCom1').html(`<option value="" disabled selected>Item 1</option>${listItems}`);
+	$('#heightCom2').html(`<option value="" disabled selected>Item 2</option>${listItems}`);
+};
 
 // calculates the conversion and updates the name to be plural if needed
 function calculateComparison() {
-	let statement = $('#unit').val();
-	let statement2 = $('#heightCom1').val();
-	let statement3 = $('#heightCom2').val();
-	let unitConversions = [];
+	let quantity = $('#unit').val();
+	let unit1 = $('#heightCom1').val();
+	let unit2 = $('#heightCom2').val();
+	let conversion = $('#unitSelection').val();
 
-	if ($('#unitSelection').val() == 'heightCompare') {
-		unitConversions = heightConversions;
-	} else if ($('#unitSelection').val() == 'weightCompare') {
-		unitConversions = weightConversions;
-	} else {
-		unitConversions = volumeConversions;
-	}
+	const calculation = quantity * conversions[conversion][unit1].size / conversions[conversion][unit2].size;
 
-	const calculation = statement * unitConversions[statement2].size / unitConversions[statement3].size;
+	// Fixes decimal point places based on whether answer is more or less than 1
 	let fixedCalculation = 0;
 	calculation > 1 ? (fixedCalculation = parseFloat(calculation.toFixed(4))) : (fixedCalculation = parseFloat(calculation.toFixed(12)));
 
 	let item1 = '';
 	let item2 = '';
-	statement == 1 ? (item1 = unitConversions[statement2].name) : (item1 = unitConversions[statement2].pluralName);
-	fixedCalculation == 1 ? (item2 = unitConversions[statement3].name) : (item2 = unitConversions[statement3].pluralName);
-	const resultS = statement + ' ' + item1 + ' is equal to ' + fixedCalculation.toString() + ' ' + item2;
-	$('#heightResults').replaceWith(`<div id="heightResults">${resultS}</div>`);
-}
+
+	console.log(quantity);
+
+	// If 1, unit should be singular. Otherwise it's plural.
+	quantity == 1 ? (item1 = conversions[conversion][unit1].name) : (item1 = conversions[conversion][unit1].pluralName);
+	fixedCalculation == 1 ? (item2 = conversions[conversion][unit2].name) : (item2 = conversions[conversion][unit2].pluralName);
+
+	const resultS = quantity + ' ' + item1 + ' is equal to ' + fixedCalculation.toString() + ' ' + item2;
+	$('#heightResults').html(`${resultS}`);
+};
